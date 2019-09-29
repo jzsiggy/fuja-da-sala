e94724f071c0838b2055ca6733f00106cc107a11
Joao Zsigmond <jzsiggy@gmail.com>
2019-09-29 14:54:14 -0300

    restructuring matrix->html translator

--- src/assets/javascript/init.js
+++ src/assets/javascript/init.js
@@ -62,19 +62,67 @@ gameSpace[avatar.y][avatar.x] = 1;
 // }
 
 const tradutor = {
-    0: "chao",
-    1: "avatar",
-    2: "sicuto",
-    3: "marcelo",
-    4: "abe",
-    5: "jesus",
-    6: "gui",
-    7: "juka",
-    8: "table",
-    9: "blackboard",
-    10: "computer",
-    11: "vase",
-    12: "door",
+    0: {
+        class: "chao",
+    },
+    1: {
+        class: "avatar",
+    },
+    2: {
+        class: "sicuto",
+        collectible: [],
+        message: "",
+    },
+    3: {
+        class: "marcelo",
+        collectible: [],
+        message: "",
+    },
+    4: {
+        class: "abe",
+        collectible: ["html"],
+        message: "salve",
+    },
+    5: {
+        class: "jesus",
+        collectible: [],
+        message: "",
+    },
+    6: {
+        class: "gui",
+        collectible: [],
+        message: "",
+    },
+    7: {
+        class: "juka",
+        collectible: [],
+        message: "",
+    },
+    8: {
+        class: "table",
+        collectible: [],
+        message: "",
+    },
+    9: {
+        class: "blackboard",
+        collectible: [],
+        message: "",
+    },
+    10: {
+        class: "computer",
+        collectible: [],
+        message: "",
+    },
+    11: {
+        class: "vase",
+        collectible: [],
+        message: "",
+    },
+    12: {
+        class: "door",
+        collectible: [],
+        message: "",
+    },
 }
 
 const createRow = (arrayOfBlocks) => {
@@ -93,7 +141,7 @@ const createRow = (arrayOfBlocks) => {
         element.style.width = `${blockWidth}px`;
         element.style.height = `${blockWidth}px`;
         element.classList.add("block");
-        element.classList.add(`${tradutor[blockNumber]}`);
+        element.classList.add(`${tradutor[blockNumber]["class"]}`);
 
         row.appendChild(element)
     });
