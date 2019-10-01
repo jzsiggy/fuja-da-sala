218233630b4dde19f66c089a4324008cd7ef37f8
Joao Zsigmond <jzsiggy@gmail.com>
2019-10-01 20:46:44 -0300

    Adding special interactions

---
+++ src/assets/javascript/special.js
@@ -0,0 +1,11 @@
+let parseBlackBoard = () => {
+    console.log("at the blackboard");
+}
+
+let parseComputer = () => {
+    console.log("at the computer");
+}
+
+let parseDoor = () => {
+    console.log("at the door");
+}
