f5a6ab3b1131e956381c0b4dd7030238fcdf954f
Joao Zsigmond <jzsiggy@gmail.com>
2019-10-02 13:43:03 -0300

    separating interaction into new file

--- src/assets/javascript/special.js
+++ src/assets/javascript/special.js
@@ -13,5 +13,5 @@ let parseBlackBoard = () => {
 
 let parseComputer = () => {
     console.log("at the computer");
-    let computerWindow = window.open("", "", "width=500,height=300");
+    let computerWindow = window.open("https://hackertyper.net/", "", "width=500,height=300");
 }
