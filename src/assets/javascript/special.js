ebf5e0a7ac4a6abcc11e92b3ff3e8d57d402a07d
Joao Zsigmond <jzsiggy@gmail.com>
2019-10-01 21:58:58 -0300

    Adding blackboard interaction

--- src/assets/javascript/special.js
+++ src/assets/javascript/special.js
@@ -1,6 +1,16 @@
 let parseBlackBoard = () => {
     console.log("at the blackboard");
-}
+    let blackboard = document.querySelector(".big-blackboard");
+    blackboard.classList.remove("hide");
+    console.log(blackboard);
+    
+    document.addEventListener('keypress', removeBlackboard = (event) => {
+        if (event.key == "q") {
+            blackboard.classList.add("hide");
+            document.removeEventListener("keypress", removeBlackboard);
+        };
+    });
+};
 
 let parseComputer = () => {
     console.log("at the computer");
