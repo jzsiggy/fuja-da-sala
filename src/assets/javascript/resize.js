9061ad7df77e114a094ad5085f97c7c34aa88c95
Joao Zsigmond <jzsiggy@gmail.com>
2019-10-04 11:15:41 -0300

    More mobile friendly

--- src/assets/javascript/resize.js
+++ src/assets/javascript/resize.js
@@ -10,6 +10,7 @@ window.addEventListener("resize", () => {
 });
 
 let moveCamera = (avatarDiv) => {
+    let gameWindow = document.querySelector("#game-window");    
     let left = avatarDiv.offsetLeft;
     let top = avatarDiv.offsetTop;
     let right = window.innerWidth - left;
@@ -18,5 +19,6 @@ let moveCamera = (avatarDiv) => {
     let height = avatarDiv.offsetHeight;
     let width = avatarDiv.offsetWidth;
 
+    gameWindow.scrollTo((left - right) / 2 + width/2 , (top - bottom) / 2 + height/2)
     window.scrollTo((left - right) / 2 + width/2 , (top - bottom) / 2 + height/2)
 };
