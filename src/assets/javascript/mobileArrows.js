90b5e2ef41c2ae39c2c3c87c444d4a799e9549fb
Joao Zsigmond <jzsiggy@gmail.com>
2019-10-05 12:50:47 -0300

    Working arrowKey functions for mobile

---
+++ src/assets/javascript/mobileArrows.js
@@ -0,0 +1,26 @@
+if (window.innerWidth <= 600) {
+    let upArrow = document.querySelector(".up");
+    let downArrow = document.querySelector(".down");
+    let leftArrow = document.querySelector(".left");
+    let rightArrow = document.querySelector(".right");
+
+    upArrow.addEventListener("click", () => {
+        console.log("up");
+        avatar.moveUp();
+    });
+
+    downArrow.addEventListener("click", () => {
+        console.log("down");
+        avatar.moveDown();
+    });
+
+    leftArrow.addEventListener("click", () => {
+        console.log("left");
+        avatar.moveLeft();
+    });
+
+    rightArrow.addEventListener("click", () => {
+        console.log("right");
+        avatar.moveRight();
+    });
+}
