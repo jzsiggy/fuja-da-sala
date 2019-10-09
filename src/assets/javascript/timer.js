7491f9f7d865d475956e8abf21fb758fed1df213
Joao Zsigmond <jzsiggy@gmail.com>
2019-10-08 21:37:33 -0300

    adding timer

---
+++ src/assets/javascript/timer.js
@@ -0,0 +1,43 @@
+class Timer {
+    constructor(minutes, seconds) {
+        this.minutes = minutes;
+        this.seconds = seconds;
+    };
+    show() {
+        let roomSpace = document.querySelector("#room-space");
+        let timerDiv = document.createElement("div");
+        timerDiv.style.position = "fixed";
+        timerDiv.style.top = "0";
+        timerDiv.style.margin = "5%";
+        timerDiv.style.color = "white";
+        timerDiv.style.fontWeight = "bolder";
+        timerDiv.style.fontSize = "3rem";
+        timerDiv.style.fontFamily = "Turret road";
+        roomSpace.appendChild(timerDiv);
+
+        this.div = timerDiv;
+    }
+
+    updateSeconds() {
+        this.seconds--;
+        if (this.seconds == 0) {
+            this.seconds = 59;
+            this.minutes--;
+        };
+        this.div.innerText = `00 : 0${this.minutes} : ${doubleDigits(this.seconds)}`;
+    };
+};
+
+let doubleDigits = (seconds) => {
+    if (seconds < 10) {
+        return `0${seconds}`;
+    } else {
+        return seconds;
+    };
+};
+
+let timer = new Timer(4, 20);
+timer.show();
+setInterval(() => {
+    timer.updateSeconds();
+}, 1000);
